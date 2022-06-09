ab66ad288256da452b902af6dd8b345e6072c6cf
Nick Adams <nick.pappas.adams@gmail.com>
2022-06-09 12:49:21 -0500

    asking the user their name

--- src/Intro.java
+++ src/Intro.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Intro {
 
     public void intro() {
-         System.out.println("test intro");
+        // instance of a new scanner class
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Whats your name?");
+        String userInput = scanner.nextLine();
+        System.out.println("Hello " + userInput + ", nice to meet you!");
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,11 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // new instances of classes
         Intro intro = new Intro();
         LevelOne levelOne = new LevelOne();
 
+        // implementing new classes instances
         intro.intro();
         levelOne.levelOne();
     }
