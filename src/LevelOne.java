3d36df8b6756a8672f057452c1605d8aefea92c8
Nick Adams <nick.pappas.adams@gmail.com>
2022-06-09 14:23:56 -0500

    refactored intro and added level one set up

--- src/LevelOne.java
+++ src/LevelOne.java
@@ -1,7 +1,35 @@
+import java.util.Scanner;
 
 public class LevelOne {
 
-    public void levelOne() {
-        System.out.println("level one test");
+    public void levelOne(String name) {
+        boolean confirm;
+        Scanner scanner = new Scanner(System.in);
+        do {
+        System.out.println(name + ", You start your journey at a crossroads, you can go north, south, east, or west, which way will you go?[N/S/E/W]");
+        String response = scanner.nextLine();
+
+            if (response.equalsIgnoreCase("N")) {
+                System.out.println("North");
+            } else if (response.equalsIgnoreCase("S")) {
+                System.out.println("south");
+            } else if (response.equalsIgnoreCase("E")) {
+                System.out.println("east");
+            } else if (response.equalsIgnoreCase("W")) {
+                System.out.println("West");
+            } else {
+                System.out.println("please enter a valid response");
+            }
+            System.out.println("try again?[y/n]");
+            String res = scanner.nextLine();
+            if(res.equalsIgnoreCase("Y")) {
+                confirm = true;
+            } else {
+                confirm = false;
+                System.out.println("Thanks for playing!");
+            }
+
+        } while(confirm);
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,30 @@
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
         // new instances of classes
-        Intro intro = new Intro();
+
         LevelOne levelOne = new LevelOne();
 
-        // implementing new classes instances
-        intro.intro();
-        levelOne.levelOne();
+        // intro asking user for their name
+        String name = "";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Whats your name?");
+        name = scanner.nextLine();
+        System.out.println("Hello " + name + ", nice to meet you!");
+
+        System.out.println(name + " do you want to play a game?[y/n]");
+        String res = scanner.nextLine();
+        if(res.equalsIgnoreCase("y")) {
+            levelOne.levelOne(name);
+        } else {
+            System.out.println("maybe next time!");
+        }
+
+
+
+
     }
 }
