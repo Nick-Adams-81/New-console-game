38773b209d471e22a474a47171a66be658cb33cb
Nick Adams <nick.pappas.adams@gmail.com>
2022-06-10 12:20:13 -0500

    added hero attributes to east direction

--- src/LevelOneDirections/East.java
+++ src/LevelOneDirections/East.java
@@ -2,7 +2,8 @@ package LevelOneDirections;
 
 public class East {
 
-    public void east(String name) {
+    public void east(String name, String hero, int heroHealthPoints, int heroAttackPoints, int potions) {
         System.out.println(name + ", heading east may be dangerous, be very careful on your journey!");
+        System.out.println(hero + " " + heroHealthPoints + " " + heroAttackPoints + " " + potions);
     }
 }
